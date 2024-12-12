a7662f6b184b8a9f787b6a20bf86351ab5a0aeec
AnlangA <2682525840@qq.com>
2024-12-12 17:57:47 +0800

    add: reference example

---
+++ src/input/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    string name;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello " << name << ", you entered: " << number << endl;
+
+    return 0;
+}
---
+++ src/reference/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 5, b = 10;
+
+    std::cout << "Before swap - a: " << a << ", b: " << b << std::endl;
+
+    swap(a, b);
+
+    std::cout << "After swap - a: " << a << ", b: " << b << std::endl;
+
+    // Reference to variable a
+    int& ref = a;
+    ref = 20; // Changes value of a
+
+    std::cout << "After reference change - a: " << a << std::endl;
+
+
+    /* int& a; // error: uninitialized reference
+    a = 12;
+    std::cout << a;
+    */
+    return 0;
+}
